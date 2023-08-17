d3f86f81b126abed28f4d0f9ebf1e21856de7f54
ParkShinYul <ehszl409@naver.com>
2023-08-17 16:18:01 +0900

    helloworld

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
 
+	@RequestMapping("/")
+	String home() {
+		return "<h1>hello woong World</h1>";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
