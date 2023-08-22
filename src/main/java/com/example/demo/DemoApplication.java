c37b7e0a1d141ecb133eb5fcd846871618aa8162
ParkShinYul <ehszl409@naver.com>
2023-08-22 21:16:02 +0900

    mioracle

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,16 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.ui.Model;
 
 @SpringBootApplication
 @RestController
 public class DemoApplication {
 
 	@RequestMapping("/")
-    public String home(Model model) {
-        return "index";
-    }
+	String home() {
+		return "<h1>hello woong World</h1>";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,20 +1,13 @@
 package com.example.demo;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.beans.factory.annotation.Autowired;
+
 @SpringBootTest
 class DemoApplicationTests {
 
-    @Autowired
-    private DemoApplication demoApplication;
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void testHome() {
-        String result = demoApplication.home(null);  // Pass null for Model
-
-        assertEquals("index", result);
-    }
 }
