ef87e514409ed36dba51832fe4a2bd19d074d684
ParkShinYul <ehszl409@naver.com>
2023-08-22 21:13:00 +0900

    miracle

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,15 +1,20 @@
 package com.example.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import org.springframework.beans.factory.annotation.Autowired;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-		// This method is intended to be overridden in a subclass.
-   		// Implementation details will be added in the future.
-	}
+    @Autowired
+    private DemoApplication demoApplication;
 
+    @Test
+    void testHome() {
+        String result = demoApplication.home(null);  // Pass null for Model
+
+        assertEquals("index", result);
+    }
 }
