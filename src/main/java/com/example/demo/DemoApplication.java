6f8287f59db04af1dc74b26f935c33da11f0e8bb
ParkShinYul <ehszl409@naver.com>
2023-08-24 22:04:02 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -11,10 +11,7 @@ public class DemoApplication {
 
 	@RequestMapping("/")
 	String home() {
-		return "<div style=\"position: relative; width: 100%; height: 0; padding-top: 56.25%; overflow: hidden; will-change: transform;\">\r\n" + //
-				"            <iframe loading=\"lazy\" style=\"position: absolute; width: 100%; height: 100%; top: 0; left: 0; border: none; padding: 0; margin: 0;\" src=\"https:&#x2F;&#x2F;www.miricanvas.com&#x2F;v&#x2F;12c5vx2?embed\">\r\n" + //
-				"            </iframe>\r\n" + //
-				"        </div>";
+		return "<h1>hello Megazone cloud</h1>";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
